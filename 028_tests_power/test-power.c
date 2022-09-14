222d6570115fb292176005b48f512d2ebdc85708
jz423 <jz423@duke.edu>
2022-09-14 01:01:37 -0400

    Did assignment 28

--- 028_tests_power/test-power.c
+++ 028_tests_power/test-power.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 unsigned power(unsigned, unsigned);
-void run_check(unsigned x, unsigned y, unsigned expected_ans) {
+void power_check(unsigned x, unsigned y, unsigned expected_ans) {
   if (power(x, y) != expected_ans) {
     printf("The output of power function is not correct!");
     exit(EXIT_FAILURE);
@@ -9,13 +9,13 @@ void run_check(unsigned x, unsigned y, unsigned expected_ans) {
 }
 
 int main() {
-  run_check(0, 0, 1);
-  run_check(1, 0, 1);
-  run_check(0, 1, 0);
-  run_check(2, 2, 4);
-  run_check(2, 3, 8);
-  run_check(3, 2, 9);
-  run_check(10, 0, 1);
-  run_check(5, 5, 25);
+  power_check(0, 0, 1);
+  power_check(1, 0, 1);
+  power_check(0, 1, 0);
+  power_check(2, 2, 4);
+  power_check(2, 3, 8);
+  power_check(3, 2, 9);
+  power_check(10, 0, 1);
+  power_check(5, 5, 25);
   return EXIT_SUCCESS;
 }
