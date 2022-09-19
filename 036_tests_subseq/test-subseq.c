18d78a060d80bc422aef1f44876bcd3354d61be8
jz423 <jz423@duke.edu>
2022-09-19 01:12:34 -0400

    Did assignment 36

--- 036_tests_subseq/test-subseq.c
+++ 036_tests_subseq/test-subseq.c
@@ -13,7 +13,6 @@ int main() {
   helper((int[]){}, 0, 0);
   helper((int[]){1}, 1, 1);
   helper((int[]){1, 1}, 2, 1);
-  helper((int[]){1, 2, 1}, 3, 2);
   helper((int[]){1, 2}, 2, 2);
   helper((int[]){2, 5, 1, 2, 3, 53, 2, 2, 3}, 9, 4);
   helper((int[]){-2147483648, 2147483647}, 2, 2);
