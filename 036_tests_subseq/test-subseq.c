7844b3efda8ff01ca06f373e5bd8096d311a8931
jz423 <jz423@duke.edu>
2022-09-19 01:04:18 -0400

    Did assignment 36

---
+++ 036_tests_subseq/test-subseq.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n);
+
+void helper(int array[], size_t n, size_t ans) {
+  if (maxSeq(array, n) != ans) {
+    printf("Your answer is not correct!");
+    exit(EXIT_FAILURE);
+  }
+}
+int main() {
+  helper((int[]){}, 0, 0);
+  helper((int[]){1}, 1, 1);
+  helper((int[]){1, 2, 1}, 3, 2);
+  helper((int[]){1, 2}, 2, 2);
+  helper((int[]){2, 5, 1, 2, 3, 53, 2, 2, 3}, 9, 4);
+
+  return EXIT_SUCCESS;
+}
