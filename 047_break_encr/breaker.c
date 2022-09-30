cc5b1a22964f76c7ef126c8e68e5bfd4789b1450
jz423 <jz423@duke.edu>
2022-09-30 13:37:49 -0400

    Did assignment 47

--- 047_break_encr/breaker.c
+++ 047_break_encr/breaker.c
@@ -1,24 +1,6 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-int getKey(FILE * f);
-int main(int argc, char * argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Your input is not correct");
-    exit(EXIT_FAILURE);
-  }
-  FILE * f = fopen(argv[1], "r");
-  if (f == NULL) {
-    fprintf(stderr, "There is no file or the file name you input is incorrect");
-    exit(EXIT_FAILURE);
-  }
-  int key = getKey(f);
-  fprintf(stdout, "%d\n", key);
-  fclose(f);
-  return 0;
-}
-
 int getKey(FILE * f) {
   int c;
   int key = 0;
@@ -34,4 +16,19 @@ int getKey(FILE * f) {
     }
   }
   return (key + 22) % 26;
+}
+int main(int argc, char * argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Your input is not correct");
+    exit(EXIT_FAILURE);
+  }
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    fprintf(stderr, "There is no file or the file name you input is incorrect");
+    exit(EXIT_FAILURE);
+  }
+  int key = getKey(f);
+  fprintf(stdout, "%d\n", key);
+  fclose(f);
+  return 0;
 }
