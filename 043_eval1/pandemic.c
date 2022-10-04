31becbcb2a61fe84dad513f063bd63b37d4eb7ae
jz423 <jz423@duke.edu>
2022-10-04 10:45:40 -0400

    Final version

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -29,7 +29,7 @@ country_t parseLine(char * line) {
   }
   line++;  // We want line points to the first digit.
   // If there is a comma after name, then checking if the next character is digit
-  while (*line == '\t') {
+  while (*line == ' ') {
     line++;
   }
   if (line == NULL || *line == '\0') {
