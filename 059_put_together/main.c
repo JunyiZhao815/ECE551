d94bb28c06347a9d021a4fd83f13a4f07bad5e4f
jz423 <jz423@duke.edu>
2022-10-15 22:30:18 -0400

    59

--- 059_put_together/main.c
+++ 059_put_together/main.c
@@ -1,38 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "kv.h"
+
 #include "counts.h"
+#include "kv.h"
 #include "outname.h"
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
   //WRITE ME
-  return NULL;
+  FILE * f;
+  char * line = NULL;
+  ssize_t len = 0;
+  size_t sz = 0;
+  f = fopen(filename, "r");
+  counts_t * c = createCounts();
+  while ((len = getline(&line, &sz, f)) >= 0) {
+    if (line[len - 1] == '\n') {
+      line[len - 1] = '\0';
+    }
+    addCount(c, lookupValue(kvPairs, line));
+  }
+  free(line);
+  fclose(f);
+  return c;
 }
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
-
- //count from 2 to argc (call the number you count i)
-
+  //read the key/value pairs from the file named by argv[1] (call the result kv)
+  if (argc <= 1) {
+    fprintf(stderr, "No file read");
+    exit(EXIT_FAILURE);
+  }
+  kvarray_t * kv = readKVs(argv[1]);
+  //count from 2 to argc (call the number you count i)
+  for (int i = 2; i < argc; i++) {
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
     //   (call this result c)
-
+    counts_t * c = countFile(argv[i], kv);
     //compute the output file name from argv[i] (call this outName)
-
-
+    char * outName = computeOutputFileName(argv[i]);
     //open the file named by outName (call that f)
-
+    FILE * f;
+    if ((f = fopen(outName, "w")) == NULL) {
+      fprintf(stderr, "Cannot open file");
+      exit(EXIT_FAILURE);
+    }
     //print the counts from c into the FILE f
-
+    printCounts(c, f);
     //close f
-
+    fclose(f);
     //free the memory for outName and c
-
-
-
- //free the memory for kv
-
+    free(outName);
+    freeCounts(c);
+  }
+  //free the memory for kv
+  freeKVs(kv);
   return EXIT_SUCCESS;
 }
