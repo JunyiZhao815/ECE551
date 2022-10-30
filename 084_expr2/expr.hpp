7cad7b0a7f9a3e1e85a2b2a7f90c17902ab51f50
jz423 <jz423@duke.edu>
2022-10-30 16:46:56 -0400

    84

---
+++ 084_expr2/expr.hpp
@@ -0,0 +1,72 @@
+#include <string.h>
+
+#include <iostream>
+#include <sstream>
+class Expression {
+ public:
+  virtual std::string toString() const = 0;
+  virtual ~Expression() {}
+};
+
+class NumExpression : public Expression {
+ public:
+  long num;
+  NumExpression(long n) : num(n) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << num;
+    return ans.str();
+  }
+};
+
+class PlusExpression : public Expression {
+ public:
+  Expression * a;
+  Expression * b;
+  PlusExpression(Expression * lhs, Expression * rhs) : a(lhs), b(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << "(" << a->toString() << " + " << b->toString() << ")";
+    return ans.str();
+  }
+  ~PlusExpression() {
+    delete a;
+    delete b;
+  }
+};
+
+class MinusExpression {
+ public:
+  Expression * l;
+  Expression * r;
+  MinusExpression(Expression * lhs, Expression * rhs) : l(lhs), r(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << "(" << l->toString() << " - " << r->toString() << ")";
+    return ans.str();
+  }
+};
+
+class TimesExpression {
+ public:
+  Expression * l;
+  Expression * r;
+  TimesExpression(Expression * lhs, Expression * rhs) : l(lhs), r(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << "(" << l->toString() << " * " << r->toString() << ")";
+    return ans.str();
+  }
+};
+
+class DivExpression {
+ public:
+  Expression * l;
+  Expression * r;
+  DivExpression(Expression * lhs, Expression * rhs) : l(lhs), r(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << "(" << l->toString() << " / " << r->toString() << ")";
+    return ans.str();
+  }
+};
