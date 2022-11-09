85612beb568143b1afd04a1cd588cbcd183bae26
jz423 <jz423@duke.edu>
2022-11-09 01:42:58 -0500

    92

--- 092_lls/ll.hpp
+++ 092_lls/ll.hpp
@@ -5,7 +5,164 @@
 
 #include <cstdlib>
 #include <exception>
+#include <iostream>
 
 //YOUR CODE GOES HERE
+
+template<typename T>
+class LinkedList {
+ private:
+  class Node {
+   public:
+    T data;
+    Node * next;
+    Node * prev;
+    Node(T d) : data(d), next(NULL), prev(NULL){};
+  };
+
+ private:
+  Node * head;
+  Node * tail;
+  int size;
+
+ public:
+  LinkedList() : head(NULL), tail(NULL), size(0) {}
+  ~LinkedList() {
+    while (head != NULL) {
+      Node * temp = head->next;
+      delete head;
+      head = temp;
+    }
+  }
+  LinkedList(const LinkedList & rhs) : head(NULL), tail(NULL), size(0) {
+    Node * temp = rhs.head;
+    while (temp != NULL) {
+      addBack(temp->data);
+      temp = temp->next;
+    }
+  }
+
+  LinkedList & operator=(const LinkedList & rhs) {
+    if (this != &rhs) {
+      LinkedList ll(rhs);
+      std::swap(size, ll.size);
+      std::swap(head, ll.head);
+      std::swap(tail, ll.tail);
+    }
+    return *this;
+  }
+  void addFront(const T & item) {
+    ++size;
+    Node * temp = new Node(item);
+    temp->next = head;
+    if (tail == NULL) {
+      tail = head;
+    }
+    else {
+      head->prev = temp;
+      head = temp;
+    }
+  }
+  void addBack(const T & item) {
+    size++;
+    Node * temp = new Node(item);
+    temp->prev = tail;
+    if (head == NULL) {
+      head = tail;
+    }
+    else {
+      tail->next = temp;
+      tail = temp;
+    }
+  }
+
+  bool remove(const T & item) {
+    Node * temp = head;
+    while (temp != NULL) {
+      if (temp == NULL) {
+        return false;
+      }
+      if (temp->data == item) {
+        --size;
+        if (temp->next && temp->prev == NULL) {
+          head = NULL;
+          tail = NULL;
+          delete temp;
+          return true;
+        }
+        else if (temp->prev == NULL) {
+          head = temp->next;
+          head->prev = NULL;
+          delete temp;
+          return true;
+        }
+        else if (temp->next == NULL) {
+          tail = temp->prev;
+          tail->next = NULL;
+          delete temp;
+          return true;
+        }
+        else {
+          temp->prev->next = temp->next;
+          temp->next->prev = temp->prev;
+          delete temp;
+          return true;
+        }
+      }
+      temp = temp->next;
+    }
+    return false;
+  }
+  class e : public std::exception {
+   public:
+    const char * what() const throw() { return "index is out of boundary!"; }
+  };
+  T & operator[](int index) {
+    try {
+      if (index > size || index < 0) {
+        throw e();
+      }
+    }
+    catch (e & e) {
+      std::cerr << e.what() << std::endl;
+    }
+    Node * temp = head;
+    for (int i = 0; i < size; i++) {
+      temp = temp->next;
+    }
+    return temp->data;
+  }
+  T & operator[](int index) const {
+    try {
+      if (index > size || index < 0) {
+        throw e();
+      }
+    }
+    catch (e & e) {
+      std::cerr << e.what() << std::endl;
+    }
+    Node * temp = head;
+
+    for (int i = 0; i < size; i++) {
+      temp = temp->next;
+    }
+    return temp->data;
+  }
+
+  int find(const T & item) const {
+    int index = 0;
+    Node * temp = head;
+    while (temp != NULL) {
+      if (temp->data == item) {
+        return index;
+      }
+      ++index;
+      temp = temp->next;
+    }
+    return -1;
+  }
+
+  int getSize() const { return size; }
+};
 
 #endif
---
+++ 092_lls/test-lls.cpp
@@ -0,0 +1,222 @@
+#include <assert.h>
+
+#include <cstdlib>
+#include <iostream>
+
+#include "ll.hpp"
+
+class Tester {
+  friend class LinkedList<int>;
+  //typedef LinkedList<int> IntList;
+
+ public:
+  // testing for default constructor is done for you
+  void testDefCtor() {
+    IntList il;
+    assert(il.head == NULL);
+    assert(il.tail == NULL);
+    assert(il.getSize() == 0);
+  }
+  // example of another method you might want to write
+  void testAddFront() {
+    // WRITE ME
+    IntList il;
+    il.addFront(10);
+    assert(il.tail->next == NULL);
+    assert(il.head->prev == NULL);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.head == il.tail);
+    assert(il.getSize() == 1);
+    assert(il.head->data == 10);
+
+    il.addFront(20);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.tail->next == NULL);
+    assert(il.head->prev == NULL);
+    assert(il.tail == il.head->next);
+    assert(il.head == il.tail->prev);
+    assert(il.getSize() == 2);
+    assert(il.head->data == 20);
+    assert(il.tail->data == 10);
+    il.remove(10);
+    il.remove(20);
+  }
+
+  void testAddBack() {
+    IntList il;
+    // When there is only one node
+    il.addBack(1);
+    assert(il.head != NULL);
+    assert(il.head == il.tail);
+    assert(il.getSize() == 1);
+    assert(il[0] == 1);
+    assert(il.head->data == 1);
+    assert(il.head->next == il.head->prev);
+    assert(il.head->next == NULL);
+
+    // Two nodes
+    il.addBack(2);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.getSize() == 2);
+    assert(il[0] == 1);
+    assert(il[1] == 2);
+    assert(il.head->data == 1);
+    assert(il.tail->data == 2);
+    assert(il.head->prev == NULL);
+    assert(il.tail->next == NULL);
+    assert(il.head->next->data == 2);
+    assert(il.tail->prev == il.head);
+    assert(il.head->next == il.tail);
+
+    // Three nodes
+    il.addBack(3);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.head->prev == NULL);
+    assert(il.tail->next == NULL);
+  }
+
+  void testRemove() {
+    IntList il;
+    il.addFront(1);
+    il.addFront(2);
+    // remove element from tail when 2 elements
+    bool flag = il.remove(2);
+    assert(flag == true);
+    assert(il.tail->next == NULL);
+    assert(il.head->prev == NULL);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.head == il.tail);
+    assert(il.head->next == NULL);
+    assert(il.tail->prev == NULL);
+    assert(il.getSize() == 1);
+    assert(il.head->data == 1);
+
+    // remove element from tail when three elements
+    il.addFront(2);
+    il.addFront(3);
+
+    bool flag2 = il.remove(1);
+    assert(flag2 == true);
+    assert(il.tail->next == NULL);
+    assert(il.head->prev == NULL);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.head->next == il.tail);
+    assert(il.tail->prev == il.head);
+    assert(il.getSize() == 2);
+    assert(il.head->data == 3);
+    assert(il.tail->data == 2);
+
+    // remove element from head when three elements
+    il.addBack(1);
+    bool flag3 = il.remove(3);
+    assert(flag3 == true);
+    assert(il.tail->next == NULL);
+    assert(il.head->prev == NULL);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.head->next == il.tail);
+    assert(il.tail->prev == il.head);
+    assert(il.getSize() == 2);
+    assert(il.head->data == 2);
+    assert(il.tail->data == 1);
+    // remove element from middle when three elements
+    il.addFront(3);
+    bool flag4 = il.remove(2);
+    assert(flag4 == true);
+    assert(il.tail->next == NULL);
+    assert(il.head->prev == NULL);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.head->next == il.tail);
+    assert(il.tail->prev == il.head);
+    assert(il.getSize() == 2);
+    assert(il.head->data == 3);
+    assert(il.tail->data == 1);
+    // remove element when there is only one element
+    bool flag5 = il.remove(1);
+    assert(flag5 == true);
+    assert(il.tail->next == NULL);
+    assert(il.head->prev == NULL);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.tail->prev == NULL);
+    assert(il.head->next == NULL);
+    assert(il.head == il.tail);
+    assert(il.getSize() == 1);
+    assert(il.head->data == 3);
+    // remove element if there are multiply element that is the same
+    bool flag6 = il.remove(3);
+    assert(flag6 == true);
+    assert(il.head == NULL);
+    assert(il.tail == NULL);
+    assert(il.head == il.tail);
+    assert(il.getSize() == 0);
+
+    il.addBack(1);
+    il.addBack(2);
+    il.addBack(1);
+    bool flag7 = il.remove(1);
+    assert(flag7 == true);
+    assert(il.tail->next == NULL);
+    assert(il.head->prev == NULL);
+    assert(il.head != NULL);
+    assert(il.tail != NULL);
+    assert(il.head->next == il.tail);
+    assert(il.tail->prev == il.head);
+    assert(il.getSize() == 2);
+    assert(il.head->data == 2);
+    assert(il.tail->data == 1);
+    il.remove(2);
+    il.remove(1);
+    assert(il.remove(5) == false);
+  }
+  void testThree() {
+    IntList * l1 = new IntList();
+    l1->addFront(1);
+    //check copying
+    IntList * l2 = new IntList(*l1);
+    assert(l1->head != l2->head);
+    assert(l1->tail != l2->tail);
+    //checking deleting
+    delete l2;
+    delete l1;
+    IntList * l3 = new IntList();
+    IntList * l4 = new IntList();
+    l3->addBack(1);
+    l3->addBack(2);
+    l4->addBack(1);
+    l4->addBack(2);
+    l4->addBack(3);
+
+    //check assignment
+    *l4 = *l3;
+    assert(l4->getSize() == 2);
+    assert(l3->head->data == l4->head->data);
+    assert(l3->tail->data == l4->tail->data);
+    assert(l3->head->next->data == l4->tail->data);
+    assert(l3->tail->prev->data == l4->head->data);
+    assert(l3->head != l4->head);
+    assert(l3->tail != l4->tail);
+    assert(l4->tail->next == NULL);
+    assert(l4->head->prev == NULL);
+    delete l3;
+    delete l4;
+  }
+};
+
+int main(void) {
+  Tester t;
+  t.testDefCtor();
+  t.testAddFront();
+  t.testAddBack();
+  t.testThree();
+  t.testRemove();
+  // write calls to your other test methods here
+  return EXIT_SUCCESS;
+}
