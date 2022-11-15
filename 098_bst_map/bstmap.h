b6aae96d06b1f2555f1fe366e1995fc377133306
jz423 <jz423@duke.edu>
2022-11-15 00:30:54 -0500

    98

--- 098_bst_map/bstmap.h
+++ 098_bst_map/bstmap.h
@@ -58,6 +58,32 @@ class BstMap : public Map<K, V> {
       }
     }
   }
+  Node * findNode(Node * curr, const K & key) {
+    Node * travesal = curr;
+    while (travesal != NULL) {
+      if (key == travesal->key) {
+        return travesal;
+      }
+      else if (key > travesal->key) {
+        travesal = travesal->right;
+      }
+      else {
+        travesal = travesal->left;
+      }
+    }
+    return travesal;
+  }
+
+  virtual const V & lookup(const K & key) const throw(std::invalid_argument) {
+    Node * target = findNode(this->root, key);
+    if (target == NULL) {
+      throw std::invalid_argument("key not found");
+    }
+    else {
+      return target->value;
+    }
+  }
+  /**
   virtual const V & lookup(const K & key) const throw(std::invalid_argument) {
     Node * travesal = this->root;
     Node * target = NULL;
@@ -79,52 +105,8 @@ class BstMap : public Map<K, V> {
       return target->value;
     }
   }
-  virtual void remove(const K & key) { root = removeHelper(root, key); }
-  Node * removeHelper(Node * curr, const K & key) {
-    if (curr == NULL) {
-      return curr;
-    }
-    if (curr->key == key) {
-      if (curr->left == NULL) {
-        Node * temp = curr->right;
-        delete temp;
-        return temp;
-      }
-      else if (curr->right == NULL) {
-        Node * temp = curr->left;
-        delete curr;
-        return temp;
-      }
-      else {
-        K kTarget = minKey(curr->right);
-        V vTarget = lookup(kTarget);
-        curr->right = removeHelper(curr->right, kTarget);
-        curr->key = kTarget;
-        curr->value = vTarget;
-        return curr;
-      }
-    }
-    else if (key < curr->key) {
-      curr->left = removeHelper(curr->left, key);
-      return curr;
-    }
-    else {
-      curr->right = removeHelper(curr->right, key);
-      return curr;
-    }
-  }
-  K minKey(Node * curr) {
-    assert(curr != NULL);
-    Node * travesal = curr;
-    K temp = curr->key;
-    while (travesal != NULL) {
-      temp = travesal->key;
-      travesal = travesal->left;
-    }
-    return temp;
-  }
-};
-/*
+  */
+
   virtual void remove(const K & key) { root = helper(root, key); }
   Node * helper(Node * curr, const K & key) {
     if (curr == NULL) {
@@ -172,4 +154,3 @@ class BstMap : public Map<K, V> {
     }
   }
 };
-*/
