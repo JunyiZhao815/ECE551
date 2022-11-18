8e20465ac93ddc35301c295a1d441ab52bf79636
jz423 <jz423@duke.edu>
2022-11-17 23:45:22 -0500

    98

--- 098_bst_map/bstmap.h
+++ 098_bst_map/bstmap.h
@@ -1,133 +1,163 @@
-#include <assert.h>
 
-#include <cstdio>
-#include <cstdlib>
+#include <exception>
 #include <iostream>
+#include <stdexcept>
 
 #include "map.h"
-
 template<typename K, typename V>
 class BstMap : public Map<K, V> {
  private:
   class Node {
    public:
-    K key;
-    V value;
+    K k;
+    V v;
     Node * left;
     Node * right;
-    Node(K key, V value) : key(key), value(value), left(NULL), right(NULL) {}
+    Node(K k, V v, Node * l, Node * r) : k(k), v(v), left(l), right(r) {}
   };
   Node * root;
+  Node * clone(Node * curr);
+  void destory(Node * curr);
+  void print(Node * curr) {
+    if (curr != NULL) {
+      print(curr->left);
+      std::cout << "(" << curr->k << ", " << curr->v << ") ";
+      print(curr->right);
+    }
+  }
 
  public:
-  BstMap() : root(NULL) {}
-  virtual ~BstMap<K, V>() { freeTree(root); }
-  void freeTree(Node * root) {
-    if (root == NULL) {
-      return;
-    }
-    freeTree(root->left);
-    freeTree(root->right);
-    delete root;
-  }
-  virtual void add(const K & key, const V & value) {
-    if (root == NULL) {
-      root = new Node(key, value);
-    }
-    else {
-      Node * travesal = root;
-      while (travesal != NULL) {
-        if (key > travesal->key) {
-          if (travesal->right == NULL) {
-            travesal->right = new Node(key, value);
-            break;
-          }
-          else {
-            travesal = travesal->right;
-          }
-        }
-        else {
-          if (travesal->left == NULL) {
-            travesal->left = new Node(key, value);
-            break;
-          }
-          else {
-            travesal = travesal->left;
-          }
-        }
-      }
-    }
-  }
-
-  virtual const V & lookup(const K & key) const throw(std::invalid_argument) {
-    Node * travesal = this->root;
-    Node * target = NULL;
-    while (travesal != NULL) {
-      if (key == travesal->key) {
-        target = travesal;
-      }
-      else if (key < travesal->key) {
-        travesal = travesal->left;
-      }
-      else {
-        travesal = travesal->right;
-      }
-    }
-    if (target == NULL) {
-      throw std::invalid_argument("cannot find the key");
-    }
-    else {
-      return target->value;
-    }
-  }
-
-  virtual void remove(const K & key) { root = helper(root, key); }
-  Node * helper(Node * curr, const K & key) {
-    if (curr == NULL) {
-      return curr;
-    }
-    if (curr->key == key) {
-      if (curr->left == NULL && curr->right == NULL) {
-        delete curr;
-      }
-      else if (curr->left == NULL) {
-        Node * temp = curr->right;
-        delete curr;
-        return temp;
-      }
-      else if (curr->right == NULL) {
-        Node * temp = curr->left;
-        delete curr;
-        return temp;
-      }
-      else {
-        //get min key
-        curr = curr->right;
-        if (curr == NULL) {
-          std::cerr << "Cannot find the min key" << std::endl;
-          exit(EXIT_FAILURE);
-        }
-        Node * travesal = curr;
-        K temp = curr->key;
-        while (travesal != NULL) {
-          temp = travesal->key;
-          travesal = travesal->left;
-        }
-        K k = temp;
-        V v = lookup(k);
-        curr->right = helper(curr->right, k);
-        curr->key = k;
-        curr->value = v;
-        return curr;
-      }
-    }
-    else if (key < curr->key) {
-      curr->left = helper(curr->left, key);
-      return curr;
-    }
-    else {
-      curr->right = helper(curr->right, key);
-      return curr;
-    }
+  BstMap() : root(NULL){};
+  BstMap(const BstMap & rhs);
+  BstMap & operator=(const BstMap & rhs);
+  virtual void add(const K & key, const V & value);
+  virtual const V & lookup(const K & key) const throw(std::invalid_argument);
+  virtual void remove(const K & key);
+  virtual ~BstMap<K, V>();
+  void printTree() {
+    print(root);
+    std::cout << "\n";
   }
 };
+
+// destructor helper
+template<typename K, typename V>
+void BstMap<K, V>::destory(typename BstMap<K, V>::Node * curr) {
+  if (curr != NULL) {
+    destory(curr->left);
+    destory(curr->right);
+    delete curr;
+  }
+}
+
+// destructor
+template<typename K, typename V>
+BstMap<K, V>::~BstMap() {
+  destory(root);
+  root = NULL;
+}
+
+// copy constructor helper
+template<typename K, typename V>
+typename BstMap<K, V>::Node * BstMap<K, V>::clone(typename BstMap<K, V>::Node * curr) {
+  if (curr == NULL) {
+    return NULL;
+  }
+  typename BstMap<K, V>::Node * newNode =
+      new typename BstMap<K, V>::Node(curr->k, curr->v, NULL, NULL);
+  newNode->left = clone(curr->left);
+  newNode->right = clone(curr->right);
+  return newNode;
+}
+
+// copy constructor
+template<typename K, typename V>
+BstMap<K, V>::BstMap(const BstMap<K, V> & rhs) {
+  root = clone(rhs.root);
+}
+
+// = operator
+template<typename K, typename V>
+BstMap<K, V> & BstMap<K, V>::operator=(const BstMap & rhs) {
+  if (&rhs != this) {
+    BstMap<K, V> temp(rhs);
+    std::swap(temp.root, root);
+  }
+  return *this;
+}
+
+// add
+template<typename K, typename V>
+void BstMap<K, V>::add(const K & key, const V & value) {
+  Node ** curr = &root;
+  while (*curr != NULL && key != (*curr)->k) {
+    if (key < (*curr)->k) {
+      curr = &(*curr)->left;
+    }
+    else {
+      curr = &(*curr)->right;
+    }
+  }
+  if (*curr == NULL) {
+    *curr = new Node(key, value, NULL, NULL);
+  }
+  else {
+    (*curr)->v = value;
+  }
+}
+
+// lookup
+template<typename K, typename V>
+const V & BstMap<K, V>::lookup(const K & key) const throw(std::invalid_argument) {
+  Node * curr = root;
+  while (curr != NULL) {
+    if (key < curr->k) {
+      curr = curr->left;
+    }
+    else if (key > curr->k) {
+      curr = curr->right;
+    }
+    else {
+      return curr->v;
+    }
+  }
+  throw std::invalid_argument("Invaild Key!\n");
+}
+
+// remove
+template<typename K, typename V>
+void BstMap<K, V>::remove(const K & key) {
+  Node ** curr = &root;
+  while (*curr != NULL && key != (*curr)->k) {
+    if (key < (*curr)->k) {
+      curr = &(*curr)->left;
+    }
+    else {
+      curr = &(*curr)->right;
+    }
+  }
+  if (*curr == NULL) {
+    return;
+  }
+  if ((*curr)->left == NULL) {
+    Node * temp = (*curr)->right;
+    delete *curr;
+    *curr = temp;
+  }
+  else if ((*curr)->right == NULL) {
+    Node * temp = (*curr)->left;
+    delete *curr;
+    *curr = temp;
+  }
+  else {
+    Node ** temp = &(*curr)->left;
+    while ((*temp)->right != NULL) {
+      temp = &(*temp)->right;
+    }
+    (*curr)->k = (*temp)->k;
+    (*curr)->v = (*temp)->v;
+    Node * lTemp = (*temp)->left;
+    delete *temp;
+    *temp = lTemp;
+  }
+}
