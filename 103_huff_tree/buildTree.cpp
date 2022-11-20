fd626b284b01cdc8d390902a81957f1069fd073c
jz423 <jz423@duke.edu>
2022-11-20 00:41:53 -0500

    103

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,22 @@
 #include "node.h"
-
-Node *  buildTree(uint64_t * counts) {
+using namespace std;
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue<Node *, vector<Node *>, NodePtrCompare> priority_queue_t;
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      priority_queue_t.push(new Node(i, counts[i]));
+    }
+  }
+  while (priority_queue_t.size() > 1) {
+    Node * first = priority_queue_t.top();
+    priority_queue_t.pop();
+    Node * second = NULL;
+    if (!priority_queue_t.empty()) {
+      second = priority_queue_t.top();
+      priority_queue_t.pop();
+    }
+    priority_queue_t.push(new Node(first, second));
+  }
+  return priority_queue_t.top();
+}
